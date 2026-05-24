69efd1943d257a4dd31bee42b0625937ab9c4ae6
im <94442486+algoritmikaa@users.noreply.github.com>
2026-05-24 16:22:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// your code goe// Подсказка управления во время игры
+ctx.fillStyle = 'rgba(255,255,255,0.6)';
+ctx.font = '9px "Courier New"';
+ctx.fillText('WASD = движение | Мышь = прицел | ЛКМ = стрельба', 10, H - 30);
